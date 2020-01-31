ef2f3fa0da970dc3eae561091cd15d05368a8324
Kenta Medina <kenta.medina@protonmail.com>
2020-02-01 06:59:03 +0900

    FInished main menu animation test
    It's ugly and buggy as hell, but it exists.

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+	[SerializeField] private Transform idlePosition;
+	[SerializeField] private Transform focusPosition;
+	[SerializeField] private Transform endPosition;
+	[SerializeField] private Canvas blackCanvas;
+	private CanvasGroup canvasGroup;
+
+    void Start()
+    {
+    	Camera.main.transform.position = idlePosition.position;
+    	canvasGroup = blackCanvas.GetComponent<CanvasGroup>();
+    	canvasGroup.alpha = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space) && Camera.main.transform.position != focusPosition.position) {
+        	StartCoroutine(MoveToFocus());
+        }
+        else if(Input.GetKey(KeyCode.Space) ) {
+        	StartCoroutine(MoveToEnd());
+        }
+    }
+
+    private IEnumerator MoveToFocus() {
+    	float progress = 0;
+    	while(progress < 1) {
+    		progress += Time.deltaTime;
+    		Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, focusPosition.position, Time.deltaTime);
+    		print(progress);
+    		yield return null;
+    	}
+    	//
+    }
+
+    private IEnumerator MoveToEnd() {
+    	float progress = 0;
+    	while(progress < 1) {
+    		progress += Time.deltaTime;
+    		Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, endPosition.position, progress);
+    		canvasGroup.alpha = progress;
+    		yield return null;
+    	}
+    }
+}
